4dfe51946bf5bbd3050d2d256fa375f6597d2af0
Ayan Chavand <ayan.chavand15@gmail.com>
2025-12-30 00:54:36 +0530

    Release: 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './compopnents/Home'
-import About from './compopnents/About'
-import Projects from './compopnents/Projects'
-import Navbar from './compopnents/Navbar'
-import Footer from './compopnents/Footer'
-import Experience from './compopnents/Experience'
-import Games from './compopnents/Games'
-import CustomCursor from './compopnents/CustomCursor'
-import DefaultTransition from './compopnents/DefaultTransition'
+import { useState } from "react";
+import "./App.css";
+import Home from "./compopnents/Home";
+import About from "./compopnents/About";
+import Projects from "./compopnents/Projects";
+import Navbar from "./compopnents/Navbar";
+import Footer from "./compopnents/Footer";
+import Experience from "./compopnents/Experience";
+import DefaultTransition from "./compopnents/DefaultTransition";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-   return (
-     <div className="font-sans text-gray-800">
+  return (
+    <div className="font-mono text-gray-800">
       <DefaultTransition duration={1500} />
       <Navbar />
       <Home />
       <Experience />
       <Projects />
       <About />
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
